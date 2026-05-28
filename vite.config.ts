ee398433d0a1f50ed9991780ffb4ad444d047689
jas-design <andres@jasperdesign.com.br>
2026-05-28 17:30:01 -0300

    update gitpages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.VITE_BASE_URL ?? './',
+    base: './',
     plugins: [react(), tailwindcss(), svgr()],
     resolve: {
       alias: {
